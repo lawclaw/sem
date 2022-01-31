0dd0b4f3e9a4b70879d3e1cf6e8e593d1a9b2168
Vinh Phat Tu <vphatmail@gmail.com>
2022-01-31 19:08:51 +0000

    added Employee class and getEmployee method.

--- src/main/java/com/napier/sem/Employee.java
+++ src/main/java/com/napier/sem/Employee.java
@@ -1,2 +1,11 @@
-package com.napier.sem;public class Employee {
+package com.napier.sem;
+
+public class Employee {
+    public int emp_no;
+    public String first_name;
+    public String last_name;
+    public String title;
+    public int salary;
+    public String dept_name;
+    public String manager;
 }
